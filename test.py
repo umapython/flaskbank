4bbdf7df2f61e3dd74ef868061953a213a98a5f7
uma <pdxuma@gmail.com>
2022-12-29 15:54:16 -0800

    seconf commit

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 rm -rf .git
+
+git init
+git add .
+git commit -m "first commit"
+git remote add origin "remote url"
+If the GitHub repo has seen new commits pushed to it, while you were working locally, I would advise using:
+git pull --rebase origin main
+git push origin main
